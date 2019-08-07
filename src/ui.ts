828456ea88c5a7391b0b2b4881352a87d844c62c
Matan Kushner <hello@matchai.me>
2019-08-07 17:12:44 -0400

    Show JSON preview

--- src/ui.ts
+++ src/ui.ts
@@ -1,16 +1,21 @@
 import "./ui.css";
 
-document.getElementById("populate").onclick = async () => {
+document.querySelector("form").onsubmit = handleSubmit;
+
+async function handleSubmit(event) {
+  // Prevent form submission
+  event.preventDefault();
+
   const textbox = document.getElementById("url") as HTMLInputElement;
   const url = String(textbox.value);
 
   const res = await fetch(url);
   if (!res.ok) return; // TODO: Handle error
-  
-  const body = await res.json();
-  parent.postMessage({ pluginMessage: { type: "populate", body } }, "*");
-};
 
-document.getElementById("undo").onclick = () => {
-  parent.postMessage({ pluginMessage: { type: "undo" } }, "*");
-};
+  const body = await res.json();
+
+  parent.postMessage({ pluginMessage: { type: "populate", body } }, "*");
+
+  const jsonPreview = document.getElementById("json-preview");
+  jsonPreview.innerText = JSON.stringify(body);
+}
